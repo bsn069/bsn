ffa25a2c0fbfb2eae7bea27fd990d681c161177e
putao <513026809@qq.com>
2016-11-10 20:25:02 +0800

    setup

--- setup.bat
+++ setup.bat
@@ -1,15 +1,84 @@
+@echo off
+
 pushd nogit
-git clone https://github.com/topameng/tolua.git
+if exist tolua (
+	echo tolua had found
+) else (
+	echo clone tolua from https://github.com/topameng/tolua.git
+	git clone https://github.com/topameng/tolua.git
+)
 popd
-pause
 
+pushd u3d\Assets\Bsn\ThirdPart\tolua
+	echo clear %cd%
+	if exist Luajit (
+		echo remove %cd%\LuaJit  
+		rmdir Luajit /S /Q
+	) else (
+		echo had remove %cd%\LuaJit 
+	)
 
+	if exist Unity5.x (
+		echo remove %cd%\Unity5.x
+		rmdir Unity5.x /S /Q
+	) else (
+		echo had remove %cd%\Unity5.x
+	)
 
+	pushd Assets
+		if exist Editor (
+			echo remove %cd%\Editor
+			rmdir Editor /S /Q
+		) else (
+			echo had remove %cd%\Editor
+		)
 
+		if exist Lua (
+			echo remove %cd%\Lua
+			rmdir Lua /S /Q
+		) else (
+			echo had remove %cd%\Lua
+		)
 
+		pushd Source
+			if exist LuaConst.* (
+				echo remove %cd%\LuaConst.*
+				del LuaConst.* /S /Q
+			) else (
+				echo had remove %cd%\LuaConst.*
+			)
+		popd
 
-GOBIN=E:\github\bsn\gopath\bin
-GOPATH=E:\github\bsn\gopath
-GOROOT=D:\Software\Go17
+		pushd ToLua
+			if exist Examples (
+				echo remove %cd%\Examples
+				rmdir Examples /S /Q
+			) else (
+				echo had remove %cd%\Examples
+			)
 
-go get github.com/astaxie/beego
+			pushd Editor
+				if exist ToLuaExport.* (
+					echo remove %cd%\ToLuaExport.*
+					del ToLuaExport.* /S /Q
+				) else (
+					echo had remove %cd%\ToLuaExport.*
+				)
+
+				if exist ToLuaMenu.* (
+					echo remove %cd%\ToLuaMenu.*
+					del ToLuaMenu.* /S /Q
+				) else (
+					echo had remove %cd%\ToLuaMenu.*
+				)
+			popd
+		popd
+	popd 
+
+popd
+
+rem GOBIN=E:\github\bsn\gopath\bin
+rem GOPATH=E:\github\bsn\gopath
+rem GOROOT=D:\Software\Go17
+rem 
+rem go get github.com/astaxie/beego
