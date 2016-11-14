253a5f334d4e39c95934e28edfaac1e45097a9d7
putao <513026809@qq.com>
2016-11-14 19:14:42 +0800

    REMOVE NO USE FILE

--- u3d/setup.bat
+++
@@ -1,8 +0,0 @@
-
-git clone https://github.com/topameng/tolua.git
-
-
-xcopy tolua\Assets\Plugins Assets\Plugins /S /E /Y
-xcopy tolua\Assets Assets\Resources\ToLua /S /E /Y
-rmdir Assets\Resources\ToLua\Source /S /Q
-rmdir Assets\Resources\ToLua\Editor /S /Q
