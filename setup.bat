97af7080a37218fabbf65be071d9ef4862e2ff84
putao <513026809@qq.com>
2016-11-14 19:26:47 +0800

    setup ok

--- setup.bat
+++ setup.bat
@@ -1,93 +1,38 @@
 @echo off
 
-pushd nogit
-if exist tolua (
-	echo tolua had found
-) else (
-	echo clone tolua from https://github.com/topameng/tolua.git
-	git clone https://github.com/topameng/tolua.git
-)
-popd
+echo setup tolua
+if not exist u3d\Assets\Bsn\ThirdPart\tolua (
+	pushd nogit
+	if not exist tolua (
+		echo clone tolua from https://github.com/topameng/tolua.git
+		git clone https://github.com/topameng/tolua.git
+	)
+	popd
 
-xcopy nogit\tolua u3d\Assets\Bsn\ThirdPart\tolua /E /Y /I
-
-pushd u3d\Assets\Bsn\ThirdPart\tolua
-	echo clear %cd%
-	if exist Luajit (
-		echo remove %cd%\LuaJit  
+	xcopy nogit\tolua u3d\Assets\Bsn\ThirdPart\tolua /E /Y /I
+	pushd u3d\Assets\Bsn\ThirdPart\tolua
 		rmdir Luajit /S /Q
-	) else (
-		echo had remove %cd%\LuaJit 
-	)
-
-	if exist ProjectSettings (
-		echo remove %cd%\ProjectSettings
 		rmdir ProjectSettings /S /Q
-	) else (
-		echo had remove %cd%\ProjectSettings
-	)
-
-	if exist Unity5.x (
-		echo remove %cd%\Unity5.x
 		rmdir Unity5.x /S /Q
-	) else (
-		echo had remove %cd%\Unity5.x
-	)
-
-	pushd Assets
-		if exist Editor (
-			echo remove %cd%\Editor
+		pushd Assets
 			rmdir Editor /S /Q
 			del Editor.meta /Q
-		) else (
-			echo had remove %cd%\Editor
-		)
-
-		if exist Lua (
-			echo remove %cd%\Lua
 			rmdir Lua /S /Q
 			del Lua.meta /Q
-		) else (
-			echo had remove %cd%\Lua
-		)
-
-		pushd Source
-			if exist LuaConst.* (
-				echo remove %cd%\LuaConst.*
+			pushd Source
 				del LuaConst.* /Q
-			) else (
-				echo had remove %cd%\LuaConst.*
-			)
-		popd
-
-		pushd ToLua
-			if exist Examples (
-				echo remove %cd%\Examples
+			popd
+			pushd ToLua
 				rmdir Examples /S /Q
 				del Examples.meta /Q
-			) else (
-				echo had remove %cd%\Examples
-			)
-
-			pushd Editor
-				if exist ToLuaExport.* (
-					echo remove %cd%\ToLuaExport.*
+				pushd Editor
 					del ToLuaExport.* /Q
-				) else (
-					echo had remove %cd%\ToLuaExport.*
-				)
-
-				if exist ToLuaMenu.* (
-					echo remove %cd%\ToLuaMenu.*
 					del ToLuaMenu.* /Q
-				) else (
-					echo had remove %cd%\ToLuaMenu.*
-				)
+				popd
 			popd
 		popd
-	popd 
-
-popd
+	popd
+)
 
 rem GOBIN=E:\github\bsn\gopath\bin
 rem GOPATH=E:\github\bsn\gopath
